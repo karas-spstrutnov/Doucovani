3043a1014f9054261cdafc86dd16bf735dc12023
ViktorKaras <karas@spstrutnov.cz>
2019-10-14 16:14:32 +0200

    Zacatek

--- ZavodniHra/Program.cs
+++ ZavodniHra/Program.cs
@@ -6,7 +6,21 @@ namespace ZavodniHra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Závodní hra!");
+            // vytvoří seznam zavodniku
+            List<Zavodnik> zavodnici = new List<Zavodnici>();
+            // vytvoří závodníky a přidá je do seznamu
+            zavodnici.Add(new Zavodnik());
+            zavodnici.Add(new Zavodnik());
+            zavodnici.Add(new Zavodnik());
+            zavodnici.Add(new Zavodnik());
+
+            // vytvoří dráhu
+            Draha zavodniDraha = new Draha();
+            ZavodniDraha.PrihlasiDoZavodu(zavodnici[0]);
+
+            zavodniDraha.OdstartovatZavod();
+
         }
     }
 }
